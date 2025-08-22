c35881fed0656245062accb16071f2cd0545b24a
alamin147 <alamin.14780@gmail.com>
2025-08-22 16:42:43 +0600

    add authentication middleware to user profile route for enhanced security

--- server/src/app/routes/routes.ts
+++ server/src/app/routes/routes.ts
@@ -2,6 +2,7 @@ import express from "express";
 import { authController } from "../auth/auth.controller";
 import { UserSchema } from "../auth/auth.validate";
 import validateRequest from "../midddlewares/validate";
+import auth from "../midddlewares/auth";
 
 const router = express.Router();
 
@@ -17,6 +18,13 @@ router.post(
   "/signin",
   validateRequest(UserSchema.userSigninSchema),
   authController.signin
+);
+
+// Get user profile
+router.get(
+  "/profile",
+  auth(),
+  authController.getProfile
 );
 
 export default router;
