b44c0403042ca683ff833000ae9428262e76f98c
alamin147 <alamin.14780@gmail.com>
2025-08-22 16:25:21 +0600

    client setup

---
+++ client/src/App.tsx
@@ -0,0 +1,14 @@
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+
+import "./App.css";
+
+function App() {
+  return <div>App</div>;
+}
+
+export default App;
